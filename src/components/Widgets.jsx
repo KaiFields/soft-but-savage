4401dd38e493f5637beface86d83dc2caba5248f
kaifields34 <kaifields34@gmail.com>
2026-01-08 19:40:45 +0000

    widgets

--- src/components/Widgets.jsx
+++ src/components/Widgets.jsx
@@ -4,9 +4,16 @@ import './Widgets.css';
 function Widgets() {
   return (
     <div className="widgets">
-      <div style={{width: "340px", height: "100%", border: "1px solid #ccc"}}>
-        <p>This is a placeholder for the widgets.</p>
-      </div>
+      <iframe
+        src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2FCleverProgrammerr%2F&tabs=timeline&width=340&height=1500&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId"
+        width="340"
+        height="100%"
+        style={{ border: 'none', overflow: 'hidden' }}
+        scrolling="no"
+        frameBorder="0"
+        allowTransparency="true"
+        allow="encrypted-media"
+      ></iframe>
     </div>
   );
 }
