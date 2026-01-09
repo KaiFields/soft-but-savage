27d005f913fc691db40377c5246cb788309c449e
kaifields34 <kaifields34@gmail.com>
2026-01-09 21:43:09 +0000

    feed

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,5 +1,4 @@
-
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import './Feed.css';
 import StoryReel from './StoryReel';
 import MessageSender from './MessageSender';
