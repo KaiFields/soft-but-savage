d0475a8941eb2bfa7bcdabd7497f92c6e93da114
kaifields34 <kaifields34@gmail.com>
2026-01-09 04:03:41 +0000

    app

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,9 +1,20 @@
 import { expect, test } from 'vitest';
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import { StateProvider } from './StateProvider';
+import reducer, { initialState } from './reducer.js';
 
 test('renders brand name', () => {
-  render(<App />);
+  const loggedInState = {
+    ...initialState,
+    user: { displayName: 'Test User', email: 'test@example.com' },
+  };
+
+  render(
+    <StateProvider initialState={loggedInState} reducer={reducer}>
+      <App />
+    </StateProvider>
+  );
   const linkElement = screen.getByText(/Soft But Savage/i);
-  expect(linkElement).toBeDefined();
+  expect(linkElement).toBeInTheDocument();
 });
