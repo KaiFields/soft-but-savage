5f2440e39f578baeeb414cc86e32485f1f515221
kaifields34 <kaifields34@gmail.com>
2026-01-09 21:31:35 +0000

    dev

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -8,23 +8,20 @@
     pkgs.npm
   ];
 
-  # IDX configuration
-  idx = {
-    # The VS Code extensions to install.
-    extensions = [
-      "dbaeumer.vscode-eslint",
-      "esbenp.prettier-vscode"
-    ];
+  # The VS Code extensions to install.
+  idx.extensions = [
+    "dbaeumer.vscode-eslint",
+    "esbenp.prettier-vscode"
+  ];
 
-    # Command to run on environment startup.
-    bootstrap = "npm ci";
+  # Command to run on environment startup.
+  idx.bootstrap = "npm ci";
 
-    # The previews to run.
-    previews = {
-      web = {
-        command = "npm run start -- --port $PORT --host 0.0.0.0";
-        manager = "web";
-      };
+  # The previews to run.
+  idx.previews = {
+    web = {
+      command = "npm run start -- --port $PORT --host 0.0.0.0";
+      manager = "web";
     };
   };
 }
