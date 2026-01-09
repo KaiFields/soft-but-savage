2672e2a4acbe39fdea121dd30107ef67daff9289
kaifields34 <kaifields34@gmail.com>
2026-01-09 21:23:05 +0000

    dev

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -1,26 +1,34 @@
-{pkgs}: {
-        channel = "stable-24.05";
-        packages = [
-          pkgs.nodejs_20,
-          pkgs.npm,
-          pkgs.eslint
-        ];
-        idx.extensions = [
-          "dbaeumer.vscode-eslint"
-        ];
-        idx.previews = {
-          web = {
-            command = [
-              "npm",
-              "run",
-              "start",
-              "--",
-              "--port",
-              "$PORT",
-              "--host",
-              "0.0.0.0"
-            ];
-            manager = "web";
-          };
-        };
-      }
+{ pkgs, ... }: {
+  # The Nix channel to use.
+  channel = "stable-24.05";
+
+  # The packages to install in the environment.
+  packages = [
+    pkgs.nodejs_20
+    pkgs.eslint
+    pkgs.prettier
+  ];
+
+  # The VS Code extensions to install.
+  idx.extensions = [
+    "dbaeumer.vscode-eslint"
+    "esbenp.prettier-vscode"
+  ];
+
+  # The previews to run.
+  idx.previews = {
+    web = {
+      command = [
+        "npm"
+        "run"
+        "start"
+        "--"
+        "--port"
+        "$PORT"
+        "--host"
+        "0.0.0.0"
+      ];
+      manager = "web";
+    };
+  };
+}
