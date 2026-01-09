0a70281e93419a77d1860fc08dfc1b6a273485b6
kaifields34 <kaifields34@gmail.com>
2026-01-09 22:06:16 +0000

    dev

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -2,7 +2,7 @@
   channel = "stable-24.05";
   packages = [ pkgs.nodejs_20 pkgs.npm ];
   idx.extensions = [ "dbaeumer.vscode-eslint" "esbenp.prettier-vscode" ];
-  idx.bootstrap = [ "npm install" ];
+  idx.bootstrap = [ "npm" "install" ];
   idx.previews = {
     web = {
       command = [ "npm" "run" "start" "--" "--port" "$PORT" "--host" "0.0.0.0" ];
