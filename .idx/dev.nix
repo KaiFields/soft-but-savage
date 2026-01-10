8330d7daf572f94c2ae169315ca2ab6bed876f22
kaifields34 <kaifields34@gmail.com>
2026-01-10 20:23:48 +0000

    commit

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -1,8 +1,17 @@
 { pkgs, ... }: {
   channel = "unstable";
   packages = [ pkgs.nodejs_20 pkgs.npm ];
-  idx.extensions = [ "dbaeumer.vscode-eslint" "esbenp.prettier-vscode" ];
-  idx.bootstrap = [ "npm" "install" ];
-  idx.previews.web.command = [ "npm" "run" "start" "--" "--port" "$PORT" "--host" "0.0.0.0" ];
-  idx.previews.web.manager = "web";
+  idx = {
+    extensions = [
+      "dbaeumer.vscode-eslint"
+      "esbenp.prettier-vscode"
+    ];
+    bootstrap = [ "npm" "install" ];
+    previews = {
+      web = {
+        command = [ "npm" "run" "start" "--" "--port" "$PORT" "--host" "0.0.0.0" ];
+        manager = "web";
+      };
+    };
+  };
 }
