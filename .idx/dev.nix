94b9f777d87903563e5991f07f7ecd858c9b079d
kaifields34 <kaifields34@gmail.com>
2026-01-10 20:19:04 +0000

    commit

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -11,7 +11,9 @@
       "dbaeumer.vscode-eslint"
       "esbenp.prettier-vscode"
     ];
+
     bootstrap = [ "npm" "install" ];
+
     previews = {
       web = {
         command = [ "npm" "run" "start" "--" "--port" "$PORT" "--host" "0.0.0.0" ];
