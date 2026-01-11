0291b0e7d7fb1fcc402fad74c4f5edd2448610cf
kaifields34 <kaifields34@gmail.com>
2026-01-11 13:49:24 +0000

    dev

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -8,9 +8,12 @@
     "esbenp.prettier-vscode"
   ];
 
-  idx.bootstrap = [ "npm" "install" ];
+  idx.bootstrap = [ "npm install" ];
 
-  idx.previews.web.command = [ "npm" "run" "start" "--" "--port" "$PORT" "--host" "0.0.0.0" ];
-
-  idx.previews.web.manager = "web";
+  idx.previews = {
+    web = {
+      command = [ "npm" "run" "start" "--" "--port" "$PORT" "--host" "0.0.0.0" ];
+      manager = "web";
+    };
+  };
 }
