69914e20416644717c17fe2f42325c100c5b8e35
kaifields34 <kaifields34@gmail.com>
2026-01-11 13:45:32 +0000

    commit

--- src/index.jsx
+++ src/index.jsx
@@ -1,19 +1,20 @@
 
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { StateProvider } from './StateProvider.jsx';
 import reducer, { initialState } from './reducer.js';
 
-ReactDOM.render(
+const container = document.getElementById('root');
+const root = createRoot(container);
+root.render(
   <React.StrictMode>
     <StateProvider initialState={initialState} reducer={reducer}>
       <App />
     </StateProvider>
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
 
 reportWebVitals();
